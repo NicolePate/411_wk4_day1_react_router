ec11153f109264051387d24b852c06f00beb2fd4
NicolePate <nicole.pate512@gmail.com>
2020-10-20 21:05:25 -0500

    doneee

--- src/Router.js
+++ src/Router.js
@@ -7,11 +7,25 @@ to the About component.
 
 Remember to export the component at the end
 */
-
 import React from 'react'
-import { Switch, Route } from 'react-router'
+import { Switch, Route } from 'react-router-dom'
+import Home from '../src/components/Home'
+import About from '../src/components/About'
+import Car from '../src/components/Car'
+
 // Write component imports here //
+
+const Router = () =>{
+    return(
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/about' component={About}/>
+                <Route path='/car/:id' component={Car}/>
+            </Switch>  
+    );
+    }
 
 
 
 // Start Router function here //
+export default Router;
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,11 +1,46 @@
 import React from 'react'
 import cars from '../cars.json'
-// import material ui components here //
+// import material ui components here //np
+import { Container, Paper, Chip } from '@material-ui/core'
 // Container, Paper, Chip //
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      '& > *': {
+        margin: theme.spacing(5, 10, 0, 20),
+        padding: theme.spacing(2),
+        width: theme.spacing(50),
+        maxHeight: theme.spacing(100)
+      },
+    },
+  }));
+
 
 const Car = (props) => {
+    const classes = useStyles();
+    let id = props.match.params.id
+    const car = cars.find(car => car.id === Number(id))
+    let carName = car.Name.toUpperCase();
     return (
-        <h1>A specific car</h1>
+        <Container maxWidth='md' className={classes.root} id='container'>
+        <Paper  square elevation={3}>
+            <h1>{carName}</h1>
+            <Chip label={`ID: ${id}`} />
+            <Chip label={`Name: ${carName}`}/>
+            <Chip label={`Year: ${car.Year}`}/>
+            <Chip label={`Place of Origin: ${car.Origin}`}/>
+            <Chip label={`MPG: ${car.Miles_per_Gallon}`}/>
+            <Chip label={`Cylinders: ${car.Cylinders}`}/>
+            <Chip label={`Horsepower: ${car.Horsepower}`}/>
+            <Chip label={`Weight(lbs): ${car.Weight_in_lbs}`}/>
+            <Chip label={`Acceleration: ${car.Acceleration}`}/> 
+            <Chip label={`Displacement: ${car.Displacement}`}/>              
+        </Paper>
+    </Container>
     )
 }
 
